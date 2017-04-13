c1cc7410c6c8b64dd4947ec3e239c08aba40a32f
Ömer Buğra Selvi <omerbugra97@hotmail.com>
2017-04-14 01:04:09 +0300

    Update DisplayGame.java

--- DisplayGame.java
+++ DisplayGame.java
@@ -8,30 +8,47 @@ import java.awt.geom.Ellipse2D;
 import javax.swing.*;
 
 public class DisplayGame extends JPanel implements ActionListener{
-	int x;
-	int y;
+
 	Ellipse2D.Double ball=new Ellipse2D.Double(0, 0, 20, 20);
+	double v=7;
 	public DisplayGame() {
-	Timer timer=new Timer(20,this);
+	Timer timer=new Timer(33,this);
 	timer.start();
 	
 	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2=(Graphics2D)g;
+		Ellipse2D.Double ball2= new Ellipse2D.Double(100, 100, 10, 10);
 		g2.fill(ball);
 		
+		g2.fill(ball2);
+		if(ball.getBounds().intersects(ball2.getBounds())){
+			ball.width=25;
+			ball.height=25;
+			//v=5;
+			ball2=null;
+		}
+
 		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Point p=getMousePosition();
+
 		if(p==null)return;
-		double a=(p.x-ball.x-10);
-		double b=(p.y-ball.y-10);
-		ball.x+=a*0.10;
-		ball.y+=b*0.10;
+		double dx = p.x - ball.x - ball.width/2;
+		double dy = p.y - ball.y - ball.height/2;
+		if(dx*dx+dy*dy >12){
+		double a=Math.atan2(dy, dx);
+		ball.x += v * Math.cos(a);
+		ball.y += v * Math.sin(a);}
+//		double a=(p.x-ball.x-10);
+//		double b=(p.y-ball.y-10);
+//		ball.x+=a*0.05;
+//		ball.y+=b*0.05;
 		repaint();
 		
 		
